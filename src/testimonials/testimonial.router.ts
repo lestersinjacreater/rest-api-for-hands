8b2a3baf86313461803333c1229b1cdce5e9e5c3
daniel-kav <dankavatha01@gmail.com>
2025-01-28 20:38:21 +0300

    refactor: remove adminOrUserRoleAuth middleware from create testimonial endpoint

--- src/testimonials/testimonial.router.ts
+++ src/testimonials/testimonial.router.ts
@@ -15,7 +15,6 @@ testimonialRouter.get("/testimonials/:id",  getTestimonialById);
 //create a testimonial
 testimonialRouter.post("/testimonials", 
     zValidator('json', testimonialSchema),
-    adminOrUserRoleAuth,
     createTestimonial
 );
 //delete a testimonial (admin only)
